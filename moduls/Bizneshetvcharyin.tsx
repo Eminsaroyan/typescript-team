11ddf78fc0dac5f1a14178bd40336b7af811b010
Emin Saroyan <emin.saroyan.ait@gmaile.com>
2025-10-02 14:32:41 +0400

    sarqecim  biznesi patet bajin

--- moduls/Bizneshetvcharyin.tsx
+++ moduls/Bizneshetvcharyin.tsx
@@ -2,6 +2,8 @@ import Headerone from "./Headerone";
 import Biznesnav from "./Business-nav";
 import Biznessakaginlink from "./Biznessakaginlink";
 import Breadcrumb from "./Breadcrumb";
+import Bizneshetvcharyinmas from "./Bizneshetvcharyinmas";
+import Fut from "./Fut";
 
 export default function () {
     return (
@@ -11,6 +13,8 @@ export default function () {
             <img src="https://www.telecomarmenia.am/images/menu/1/15774519405911.png" alt="" className="w-[100%]" />
             <Biznessakaginlink />
             <Breadcrumb />
+            <Bizneshetvcharyinmas />
+            <Fut />
         </>
     )
 }
---
+++ moduls/Biznessakagin.tsx
@@ -0,0 +1,52 @@
+import { NavLink, useLocation } from "react-router-dom";
+
+interface Sakagin {
+    to: string;
+    label: string;
+}
+
+export default function Biznessakagin() {
+    const location = useLocation();
+
+    const equipmentList: Sakagin[] = [
+        {
+            to: "/bizneskanxavcharyin",
+            label: "Կանխավճարային",
+        },
+        {
+            to: "/bizneshetvcharyin",
+            label: "Հետվճարային",
+        },
+    ];
+
+    return (
+        <div className="w-full border-b-[1px] mt-[10px] mb-[12px] mx-auto">
+            <ul className="flex justify-center list-none w-full">
+                {equipmentList.map(({ to, label }, index) => {
+                    
+                    const isDefaultActive =
+                        location.pathname === "/mobile" && to === "/hetvcharyin";
+
+                    return (
+                        <li key={index} className="w-[12%]">
+                            <NavLink
+                                to={to}
+                                className={({ isActive }) =>
+                                    `w-full flex text-[#000000] items-center no-underline justify-center border-b-[3px] transition-all duration-300 ${
+                                        isActive || isDefaultActive
+                                            ? "border-[#f04d4d] border-b-[3px]" 
+                                            : "border-transparent"
+                                    }`
+                                }
+                            >
+                                <p className="text-[20px] mb-[12px] font-medium">
+                                    {label}
+                                </p>
+                            </NavLink>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
