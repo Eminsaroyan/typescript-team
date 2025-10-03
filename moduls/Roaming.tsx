20c1ed4428fb8deb8bd31a6175ccc2433cb60e29
Emin Saroyan <emin.saroyan.ait@gmaile.com>
2025-10-03 16:13:24 +0400

    sarqeci roaming  bajin erkrord  mas

--- moduls/Roaming.tsx
+++ moduls/Roaming.tsx
@@ -1,9 +1,11 @@
 import Roamingmek from "./Roamingmek"
+import Roamingerku from "./Roamingerku"
 
 export default function (){
     return(
         <>
         <Roamingmek />
+        <Roamingerku />
         </>
     )
 }
---
+++ moduls/Roamingerku.tsx
@@ -0,0 +1,72 @@
+export default function Roamingerku() {
+    return (
+        <div className="bg-[#01415f]">
+            <div className="mx-auto max-w-[1260px] px-[20px]">
+                <h2 className="text-center text-[#fff] text-[60px] mb-[60px]"
+                    style={{ fontFamily: "'Team-Regular', 'Helvetica', 'Arial', 'sans-serif'" }}
+                >Ինտերնետ ռոումինգում</h2>
+                <ul className="flex flex-wrap justify-center -mx-[18px] list-none">
+                    <li className="w-[30%] px-[18px] pb-[40px] cursor-pointer relative group">
+                        <p
+                            className="text-[#ebedef] text-center pt-[20px] px-[20px] pb-[15px]"
+                            style={{ fontFamily: "'Team-Regular', 'Helvetica', 'Arial', 'sans-serif'" }}
+                        >
+                            <span className="text-[24px]">
+                                Ռոումինգ փաթեթ<br />1 ԳԲ + <br />2000 դրամ
+                            </span>
+                        </p>
+                        <div className="absolute bottom-0 left-0  w-[95%]  h-[1px] bg-[#ebedef] transition-all duration-300 group-hover:h-[6px]"></div>
+                    </li>
+
+                    <li className="w-[30%] px-[18px] pb-[40px] cursor-pointer relative group">
+                        <p
+                            className="text-[#ebedef] text-center pt-[20px] px-[20px] pb-[15px]"
+                            style={{ fontFamily: "'Team-Regular', 'Helvetica', 'Arial', 'sans-serif'" }}
+                        >
+                            <span className="text-[24px]">
+                                Ռոումինգ փաթեթ<br />3 ԳԲ <br />6000 դրամ
+                            </span>
+                        </p>
+                        <div className="absolute bottom-0 left-0 w-[95%] h-[1px] bg-[#ebedef] transition-all duration-300 group-hover:h-[6px]"></div>
+                    </li>
+
+                    <li className="w-[30%] px-[18px] pb-[40px] cursor-pointer relative group">
+                        <p
+                            className="text-[#ebedef] text-center pt-[20px] px-[20px] pb-[15px]"
+                            style={{ fontFamily: "'Team-Regular', 'Helvetica', 'Arial', 'sans-serif'" }}
+                        >
+                            <span className="text-[24px]">
+                                Ռոումինգ փաթեթ<br />7 ԳԲ <br />12000 դրամ
+                            </span>
+                        </p>
+                        <div className="absolute bottom-0 left-0 w-[95%] h-[1px] bg-[#ebedef] transition-all duration-300 group-hover:h-[6px]"></div>
+                    </li>
+
+                    <li className="w-[30%] px-[18px] pb-[40px] cursor-pointer relative group">
+                        <p
+                            className="text-[#ebedef] text-center pt-[20px] px-[20px] pb-[15px]"
+                            style={{ fontFamily: "'Team-Regular', 'Helvetica', 'Arial', 'sans-serif'" }}
+                        >
+                            <span className="text-[24px]">
+                                Ռոումինգ փաթեթ<br />12 ԳԲ <br />18000 դրամ
+                            </span>
+                        </p>
+                        <div className="absolute bottom-0 left-0 w-[95%] h-[1px] bg-[#ebedef] transition-all duration-300 group-hover:h-[6px]"></div>
+                    </li>
+
+                    <li className="w-[30%] px-[18px] pb-[40px] cursor-pointer relative group">
+                        <p
+                            className="text-[#ebedef] text-center pt-[20px] px-[20px] pb-[15px]"
+                            style={{ fontFamily: "'Team-Regular', 'Helvetica', 'Arial', 'sans-serif'" }}
+                        >
+                            <span className="text-[24px]">
+                                Ռոումինգ փաթեթ 1 ԳԲ  (Ռուսաստան, Վրաստան)<br />500 դրամ
+                            </span>
+                        </p>
+                        <div className="absolute bottom-0 left-0 w-[95%] h-[1px] bg-[#ebedef] transition-all duration-300 group-hover:h-[6px]"></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
